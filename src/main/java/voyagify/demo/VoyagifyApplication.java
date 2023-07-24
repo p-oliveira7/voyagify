d861ea9c60c8a76d9b54ea1c2a173e6d2320bb0b
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-23 21:06:11 -0300

    Initial commit

---
+++ src/main/java/voyagify/demo/VoyagifyApplication.java
@@ -0,0 +1,13 @@
+package voyagify.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyagifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyagifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/voyagify/demo/VoyagifyApplicationTests.java
@@ -0,0 +1,13 @@
+package voyagify.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyagifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
